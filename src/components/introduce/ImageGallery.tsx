f7a903d1d687ed0163fc22a919a1cd7a826afced
NguyenKietNuki <ngkietbaby19@gmail.com>
2026-04-05 19:53:02 +0700

    feat(introduce): fix imagegallery

--- src/components/introduce/ImageGallery.tsx
+++ src/components/introduce/ImageGallery.tsx
@@ -3,36 +3,38 @@ import Image from "next/image";
 import { useState, useEffect, use } from "react";
 
 export default function ImageGallery() {
-    const images  = [
+    const [images, setImages] = useState([
         "/introduce/PC_laptop.jpg",
         "/introduce/PC-1.jpg",
         "/introduce/PC_2.jpg",
         "/introduce/laptop-1.jpg",
         "/introduce/laptop-2.png",
-    ];
+    ]);
 
-     const [index, setIndex] = useState(0);
-
+    const rotateImages = () => {
+        setImages((prevImages) => {
+            const newImages =  [...prevImages];
+            const firstImage = newImages.shift();
+            newImages.push(firstImage!);
+            return newImages;
+        });
+    };
+    
     useEffect(() => {
-        const interval = setInterval(() => {
-            setIndex((prev) => (prev + 1) % images.length);
-        }, 3000);
+        const interval = setInterval(rotateImages, 3000);
         return () => clearInterval(interval);
-    }, []);
+    },[]);
 
     return (
         <div className="w-full h-full bg-gray-100 flex justify-center items-center p-4">
             <div className="flex h-auto w-auto gap-x-2.5">
                 <div className="w-[600px] h-100 relative rounded-2xl overflow-hidden">
-                    <Image src={images[index]} alt="Main display" fill className="object-cover" priority/>
+                    <Image src={images[0]} alt="Main display" fill className="object-cover" priority/>
                 </div>
                 <div className="grid grid-cols-2 grid-rows-2 gap-2">
-                {images
-                    .filter((_, i) => i !== index)
-                    .slice(0, 4)
-                    .map((src, i) => (
+                {images.slice(1).map((src,index) => (
                     <div key={index} className="relative w-50 h-50 rounded-2xl overflow-hidden">
-                        <Image src={src} alt={`sub-${i}`} fill className="object-cover"/>
+                        <Image src={src} alt={`sub-${index}`} fill className="object-cover"/>
                     </div>
                 ))}
                 </div>
