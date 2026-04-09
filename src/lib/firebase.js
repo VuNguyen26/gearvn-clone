967ec14b53bb528c5fae537f459367c3cd47e437
NguyenKietNuki <ngkietbaby19@gmail.com>
2026-04-09 16:17:12 +0700

    feat(trade-in): fix text-size

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,6 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -15,10 +14,7 @@ const firebaseConfig = {
   appId: "1:163580213267:web:95b40c7838d1be5616e7e6",
   measurementId: "G-1S6QR3X85C"
 };
-
-
 // Khởi tạo Firebase (Tránh lỗi khởi tạo 2 lần khi Next.js reload)
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-
 // Xuất các dịch vụ bạn sẽ dùng
 export const db = getFirestore(app);
